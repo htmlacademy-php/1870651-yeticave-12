2f5afdfda3f85ecd4671defd1df4ea5fd3ef2f39
R3g1tpr0cs <kreysonvv86@yandex.ru>
2021-08-14 11:41:47 +0300

    Добавление имеи пользователи и проверка if

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $user_name = 'Vladimir'; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
+	<?php
         if ($is_auth == 1){
 		<div class="user-menu__logged">
   			<p>$user_name</p>
@@ -44,6 +44,7 @@ $user_name = 'Vladimir'; // укажите здесь ваше имя
   			</li>
 		</ul>
 	}
+	?>
         </nav>
     </div>
 </header>
