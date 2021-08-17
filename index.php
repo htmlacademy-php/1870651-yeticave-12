f578f009cf95dd7f596fcbb9dc7ac8476c1d634f
R3g1tpr0cs <kreysonvv86@yandex.ru>
2021-08-17 18:20:13 +0300

    Строгое сравнение

--- index.php
+++ index.php
@@ -28,13 +28,13 @@ $user_name = 'Vladimir Kreyson'; // укажите здесь ваше имя
 
         <nav class="user-menu">
 
-            <?php if ($is_auth == TRUE): ?>
+            <?php if ($is_auth === 1): ?>
 	            <div class="user-menu__logged">
   	                <p> <?php print($user_name) ?> </p>
   	                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
   	                <a class="user-menu__logout" href="#">Выход</a>
 	            </div>
-	        <?php elseif ($is_auth == FALSE): ?>
+	        <?php else: ?>
 	            <ul class="user-menu__list">
   	                <li class="user-menu__item">
     	                <a href="#">Регистрация</a>
