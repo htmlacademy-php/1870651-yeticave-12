d1fe7e66c027e58acfb07aa8774fe43df42c44ab
KreysonVV <kreyson@keemail.me>
2021-11-10 05:05:53 +0300

    change vars

--- templates/layout.php
+++ templates/layout.php
@@ -49,7 +49,6 @@
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <!--заполните этот список из массива категорий-->
             <?php foreach ($categories as $category): ?>
                 <li class="nav__item">
                     <a href="pages/all-lots.html"><?=$category; ?></a>
