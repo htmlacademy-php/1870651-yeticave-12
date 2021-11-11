4c396091c858b4f5746ef675c88a7340cc2f93af
KreysonVV <kreyson@keemail.me>
2021-11-11 05:54:21 +0300

    start "time"

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
 require_once "helpers.php";
 $isAuth = rand(0, 1);
-
 $title = 'YetiCave';
-$userName = 'Vladimir Kreyson'; // укажите здесь ваше имя
+$userName = 'Vladimir Kreyson';
+
+//Передаём в функцию текущую дату и дату конца продажи. Возвращает оставшееся время.
+function EndOfTime(string $dateEnd){
+    $today = date('Y-m-d H:i:s');
+    $endTime = strtotime($dateEnd);
+    $howManyTimeToEnd = $endTime - $today;
+    return $howManyTimeToEnd;
+}
+
 $categories = [
     'Доски и лыжи',
     'Крепления',
@@ -18,36 +26,42 @@ $lots = [
     'category' => 'Доски и лыжи',
     'price' => 10999,
     'url_picture' => 'img/lot-1.jpg',
+    'dateOfEnd' => '2021-11-18',
     ],
     [
     'name' => 'DC Ply Mens 2016/2017 Snowboard',
     'category' => 'Доски и лыжи',
     'price' => 159999,
     'url_picture' => 'img/lot-2.jpg',
+    'dateOfEnd' => '2021-11-28',
     ],
     [
     'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
     'category' => 'Крепления',
     'price' => 8000,
     'url_picture' => 'img/lot-3.jpg',
+    'dateOfEnd' => '2021-12-11',
     ],
     [
     'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
     'category' => 'Ботинки',
     'price' => 10999,
     'url_picture' => 'img/lot-4.jpg',
+    'dateOfEnd' => '2021-12-16',
     ],
     [
     'name' => 'Куртка для сноуборда DC Mutiny Charocal',
     'category' => 'Одежда',
     'price' => 7500,
     'url_picture' => 'img/lot-5.jpg',
+    'dateOfEnd' => '2021-12-01',
     ],
     [
     'name' => 'Маска Oakley Canopy',
     'category' => 'Маска',
     'price' => 5400,
     'url_picture' => 'img/lot-6.jpg',
+    'dateOfEnd' => '2021-12-25'
     ],
 ];
 
